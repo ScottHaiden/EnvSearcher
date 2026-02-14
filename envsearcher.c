ee2a239adb0a639787831f7e9ba5d619589a35f5
Scott Haiden <scott.b.haiden@gmail.com>
2026-02-14 10:24:56 -0800

    Implement help.

--- envsearcher.c
+++ envsearcher.c
@@ -28,6 +28,51 @@ typedef struct {
     char* (*quote_fn)(char*, char*);
 } options;
 
+typedef struct {
+    char flag;
+    const char* help_text;
+    void (*action)(options*);
+} flag;
+
+static const flag* find_flag(const flag* flags, char key) {
+    for (const flag* cur = flags; cur->flag; ++cur) {
+        if (cur->flag == key) return cur;
+    }
+
+    return NULL;
+}
+
+static void options_set_delim_nul(options* o) { o->delim = '\0'; }
+static void options_set_delim_newline(options* o) { o->delim = '\n'; }
+static void options_set_quote_fn_run_printf(options* o) { o->quote_fn = &run_printf; }
+static void options_set_quote_fn_normal(options* o) { o->quote_fn = &normal; }
+static void options_set_quote_fn_hex_encode(options* o) { o->quote_fn = &hex_encode; }
+static void options_set_quote_fn_simple_escape(options* o) { o->quote_fn = &simple_escape; }
+
+static void show_help(const char* argv0, const flag* flags, int exit_code) {
+    const char* begin = argv0;
+    for (const char* cur = argv0; *cur; ++cur) {
+        if (*cur == '/') begin = cur + 1;
+    }
+
+    FILE* const out = (exit_code) ? stderr : stdout;
+
+    fprintf(out, "usage: %s", begin);
+    for (const flag* cur = flags; cur->flag; ++cur) {
+        fprintf(out, " [-%c]", cur->flag);
+    }
+
+    fprintf(out, "\n");
+    fprintf(out, "\n");
+    fprintf(out, "options:\n");
+
+    for (const flag* cur = flags; cur->flag; ++cur) {
+        fprintf(out, "  -%c: %s\n", cur->flag, cur->help_text);
+    }
+
+    exit(exit_code);
+}
+
 static options parse_args(int argc, char** argv) {
     options ret = {
         .arg_index = 1,
@@ -35,18 +80,43 @@ static options parse_args(int argc, char** argv) {
         .quote_fn = &simple_escape,
     };
 
+    const flag flags[] = {
+        {
+            .flag = 'z', .action = &options_set_delim_nul,
+            .help_text = "Use nul char to delimit entries.",
+        },
+        {
+            .flag = 'Z', .action = &options_set_delim_newline,
+            .help_text = "Use newline to delimit entries.",
+        },
+        {
+            .flag = 'q', .action = &options_set_quote_fn_run_printf,
+            .help_text = "Use printf %q to quote entries. Requires supported printf program.",
+        },
+        {
+            .flag = 'n', .action = &options_set_quote_fn_normal,
+            .help_text = "Print values as-is, no quoting.",
+        },
+        {
+            .flag = 'x', .action = &options_set_quote_fn_hex_encode,
+            .help_text = "Hex-escape values.",
+        },
+        {
+            .flag = 's', .action = &options_set_quote_fn_simple_escape,
+            .help_text = "Use simple escape (default).",
+        },
+        {},
+    };
+
     while (true) {
-        const int opt = getopt(argc, argv, "zZqnxs");
+        const int opt = getopt(argc, argv, "zZqnxsh");
         if (opt == -1) break;
-        switch (opt) {
-            case 'z': ret.delim = '\0';              break;
-            case 'Z': ret.delim = '\n';              break;
-            case 'q': ret.quote_fn = &run_printf;    break;
-            case 'n': ret.quote_fn = &normal;        break;
-            case 'x': ret.quote_fn = &hex_encode;    break;
-            case 's': ret.quote_fn = &simple_escape; break;
-            case '?': exit(2);                       break;
-        }
+        if (opt == 'h') show_help(argv[0], &flags[0], 0);
+
+        const flag* flag = find_flag(&flags[0], opt);
+        if (flag == NULL) show_help(argv[0], &flags[0], 1);
+
+        flag->action(&ret);
     }
 
     ret.arg_index = optind;
